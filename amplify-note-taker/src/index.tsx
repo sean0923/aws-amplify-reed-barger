fc3989f2af973517ecde8ea785097ded32e50415
Sean <iamseanhong@gmail.com>
2019-09-09 19:26:58 -0700

    Configure aws_exports

--- amplify-note-taker/src/index.tsx
+++ amplify-note-taker/src/index.tsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
 ReactDOM.render(<App />, document.getElementById('root'));
